e5c54bc185df8081925796c76e588f4d77b8da9e
kamilbdbelfort <k.beelidebelfort@hotmail.com>
2021-08-31 12:20:52 +0200

    Encouraging message for new students

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log(`Hi guys, Congratulations on taking this big step forward in your development.
+I am sure you can do it! Not matters which obstacles, set backs you will face during the course.
+Just remember to stay positieve, don't presseure yourself into being stressed about not understanding some parts.
+Google solutions, ask questions your classmates and teachers. They all will be more than welcome to help you out!
+Study hard, have fun, enjoy the ride and think about which opportunities will arise in front of you once you are done!`)
